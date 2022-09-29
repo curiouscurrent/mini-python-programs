fc8f600f29ccbb252a5eaf9bdbe333835953ccc8
Anusha K <95173109+curiouscurrent@users.noreply.github.com>
2022-09-29 16:11:17 +0530

    Create Save_the_princess_game.py

---
+++ Save_the_princess_game.py
@@ -0,0 +1,122 @@
+print('''
+:,,..................................,,,,i;1fLLLf1i;i1tttttttiiiiiii;;iiiii11iiiii;,.......,,,,,,,,:
+:,,............................,,,,.,,:i1fffLLLff1i1tftfttt11iii1iiiiii;;;;;;;;;iiii;;,,...,,,,,,,,:
+:,,,..............................,,,:itLLttttt11ii;iii11111iiiiiiiiii;;;;;;;;;;;i;iiiii:,..,,,,,,,,
+:,,,.........................,:;iiiii11111111111111i;i11111111i;;;:;;;i1iii;;;;;;;;;;;;;i;:,,,,,,,,,
+:,,........................,i1t1ttttfftt1111ii1ttffft11ii1i;;;;;;iii;ii111iii;;;;;;;;;;;:;;;:.,,,,,,
+:,,,.....................,;1t1ii1ttffft11i;;:;11t11iiiii;:;1ttt111111111iii;;;;;;i;;;;;;;;:;i:,,,,,,
+::,,....................,;11i;;1tttt1ii;;;;;;:;;iii;;;;;;;;i111iii;;;;;;;::::::;;;;;::;;;;;:;i:,,,,:
+::,,...................,:iii;:;i111i1iiii11111111iiii;;:::::,:;;;;;::::::::;;ii1ii;;::::;;;;;;;,,,,:
+::,,.................,:i1t111111tttttffttt1ttt1111iiiiiiiii;;::::::::::::,:;iiii;;i;;:::::;;;;;:,,,,
+::,,................,;1tt111tttttttttt111tttt111ii11111111ii;;;::,:::::,,:::::::::;;;;;;;;;;;;;;:,,:
+::,,...............,i1t1111tttttt1tt111ttt1111ii1tt11111iiii;;;;;:::,,:::::::::::::;;;;;;;;;;;;;i;,:
+::,,..............,;i11111ttt111tt11i11t1111i;i1ttiiiii;;iiii;;;;;::,,,,,,:::,,:::::::;;;::;;:;;ii,:
+::,,..............,ii1111111111111ii1111i1i;:;1t1ii;:1;;ii;i;;;;;:::::::,:::,,,,,:::::::::::::;;;;,:
+:::,..............,ii111111i11111iii111iii:,;11i;;;:;i;i;;;;;;;;:::::;ii;i:::::,,,:::::::;;::::;;;::
+:::,..............;111111ii11111iii111iii:,;i1i;;;:::;;;;;;;;;::;;;::;iiii;:;;::,,,,::::::;;;:;;;;;:
+:::,.............:111111ii11111i;ii1i;ii;,:iii;::::::::::::;;;;;;;;:::;;;;;::::::,,,,,,:,,:;;:;;;;:;
+:::,............,iii111ii11111ii;i1i:;i;:,;ii;:::::::::::::;;;;iiii;::::;:::::::::::,,,,,,,::::::::;
+:::,............:;ii11i;i1i11ii;iii;;;;:::iii::::::::::::;;;iiiiiiii;:::::::,:::::::::,,,,,::::::::;
+:::,............:;;iiiii1iiiiiii;;;;;;:::;ii;::::::::::;;;;iiiiiiiii;::::::::,,,,,::::::,,,::::::,:;
+:::,............,;;i;;iiiiii;;;;:;;;;;::;i1;:::::::;;;;;iiiiiiiiiiiiii;:::::::,,,,,:::;;:,,,:::;;::;
+:::,.............,;:;i1t11iiiiiii;;;;:::;ii;;;;;;:::;iiiiiiiiiiiiiiiiii;;:::::::,,,,:::;;:,,:::::;;:
+:::,.............:;,tGCCCffffLft111i:,:;iiiiiii;;;::::;ii11111i11iiiiiiiii;;::,,,,,,,:::;;:,,:,,::::
+:::,............:1i;00GGGCGCLft1;::::;iiiiiiiiiiiii;:,,,,:;ii111111iiiiiiiiii;:,,::,,,,::;;:,,,,,::;
+:::,............;i;18G0CLt1;::,,:;;i111111111111iiiiii;:,,,.,::;;iiii1iiiiiiiiii;:;;,,,,,;;;,,,,,::;
+:::,.............:,f0L1;;::;i11tttt111111tttt11111iiiiiii;;::,,,,,,,::;;iiiiiiiiiiii;::,,:;;:,,,::,:
+:::,..............,Cft1ttfLfffftt1111ttttttt1111111iiiiiiiiiii;;;;::,,,,,:;;iiiiiiiiiii;,,:;;,,,,,,:
+:::,..............;L111i;i1i11tttt11ttfffftt111111iiiiiiiiiiiiiiiiiii;;;;:::;;;iiiiiii11;,::;:,:,:::
+:::,.............,:;,.......,,,:;i1ttffffftt11111iiiiiiiiii;;;:::,,,,,,:::;:;;;;iiiii1i1;,::::;:;::;
+:::,............,;;:...;tfti::,,,.,;ffffLfftt1111iiiiiiiii:,,,.....,,.......,::;iiii1111i,,::::::::;
+:::,...........,;;tfi:f@@0ti:,,,:;,,1ffLLffttt111iiiiiiii:...,::::;;i11i;:,....:;iii1111i,,::,:::::;
+;::,...........,;;t0GL@881;;... .;1:1fLLLLftt1111iiiiiii;,,:;;,...,;;ifftt1i:.::;iii11111,,,,,,::::;
+:::,...........,::LG1C880;;. ... iti1fLLLLfftt111iiiiiii;;1:;,......;iiftttffi;iiiii11111,,,,,,,:::;
+;::,............,,0Cii1LCi;;,...;fittfLLLLLftt111iiiiiii:tt:;.     ,1iifffft1;;iiiii11111:,,,,,,,:::
+;::,,.....,:::::,i80Cf1iii;iiiittitftfLCCCLttt111iiiiiiiifL1;i;:,:;11;11ii;::;ii111111111;,,,:,,,,,:
+;::,,....,,::;;;:C80GCLfft111iiii111fLCCLLLftt111i1iiiiii1tt1iiiii;;::::::;;iiii111111111i,,:;:::,,,
+;::,,....,,:::;:1GGG0GCCLLfffftttfffLLLLLLLftt11111iiiii1iiiiiiii;iiiiiiiiiiiiii111111111;:,;;::::::
+;::,,......,,::;tGGGGGCCLLLLfLfLLLLLLLLLLLLft1111111111111111111111111111ii11iiiiii111111;:;;;;;;;;;
+:::,,,,,,,,:::::iGCCCCCCLLffLfLLLfffLLLLLLftt111111ttttttttttttttt111111iiiiiiiiiiiii111i;;;;;;;;;;;
+:::,,.,.,:iii;;:;CCCLLLLLffffffffffLLLLCLLftt111111tttttttttttttt111111iiiiiiii;;iiiii11i;i;;;;;;i;;
+;;::,...,:;;;;;i;LCLLLLLffLffffffLLLLCCCLfttt11111tttttttt111111111iiiiiiiiiiii;iiiii111i;i;ii;ii;:;
+;;;::,,.,,:::;;;:1CLLfffffffffffLLLLCCCLftt1111i111111111111111iiiiiii;;;;;;;;;;;iiii11ii;iiiiii;::;
+;:;;:,,.,,::;;;::,LLffffffftfffffLLLCLft1iiiiiiiii111111111iiiiiii;;;;;;;;;;;;;;;iiii1iiiiiiiii;:::;
+;::::,,,,,,:;;;;:,;LffffffffttfffLLCf111ii;;;;;iiii1iiiiiiiiiiii;;;;;;;;;;;;;;;;iiii11iiii11i;;::::;
+:::::,,,,,,::;;;;;:1LfffftfftfffffLLLttt1i;;;;;iiiiiiiiiiiii;;;;;;;;;;;;;;;;;;;;iii1i;iiii;::;;:::::
+:::::,,.,:::::::::;;tLffffffffffffLLLLLff11iiiiiiiiiiiiiiiiii;;;;;;;;;;;;;;;;;;iii1;,::::,,,:;;::::;
+:::::,,,:::::::,:::;;tLffffffffffffLCCLfttt1iiiiiiiiiiiiiiiii;;;;;;;;;;;;;;;;;iii1;:;;:::::::;;;;;;;
+;::::,::::::::;;;;;:::1LLffffffttfLCL1i;;;;;;;;iiiiiiiiiiiiiiiii;;;;;;;;;;iiiiii1;:;;;;;;;iiii;::::;
+;::::,:;:::;;;;;;;;;;:,;LLfffftttfL1,...,:,::,,,,::;iiiiiiiiiiiiii;;;;;;iiiiii1i;;;;;;;;;iiiiii;::::
+;:;:::::::;;;;;;;;;;;;;:;tCLfffttf1,,;:,,,.,........,:;iiiiiiiiiiiiiii;iiiiiii;;;;;;;;;;;;iiiiiii;;;
+::::::::::::;:;:;;:;;;;;;:iLCLLfLLCLt11ii;::::::::::::;iiiiiiiiiiiiiiiiiiiiii;;:;;;;;;;;;;;;;;;ii;;;
+::::::::::::::::::::;;;;;;:;tCCLLCCCLft1i;:::::;;;;iiiiiiiiiiiiiiiiiiiiiiii;;;;:;;;;;;;;;;;;;;;;ii:;
+;::::::::::::::::::::;;;;;;;:ifCLLLLffftt11iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;;;;;;;;;;;;;;;;;;;;;;;i;;
+::::,::,,::::::::::::;;;;;;;;::1LLLfffttt11iiiiiiiiiiiiiiiiiiiiiiiiiii;:1ii;;;i;:;;:;:::;;;;;;:;;;;;
+::,,,,,,,,,,:::::::::;;;;;;;;;:::1LLffttt11iiiiiiiiiiiiiiiiiiiiiiiiii1::1iiiiii;:::::::::;;;;::::;;;
+:,,,,,,,,,,,,,,,,,:::::::::;;::;:::1fLftt111iiiiiiiiiiiiiiiiii;;;;;ii1::1iiiiii;::::::::::;;;:::::;;
+:,,,,,,,,,,,,,,,,,::::::;::::::::::::i1tt11iiiiiiiiiiiiiii;;;;;;;;;ii1;,1iiiiiii:::::::::::::::::::;
+:,,,,,,,,,,,,,,,,:::::::::::::::::::::,,1Ltt11iiiiiiii;;;;;;;;;;;;iii11;iiiiiiii:::::::::::::::::::;
+:,,,.,,,,.,,,,,,,::::::::::::::::::::::,;GLfffttt11ii;;;;;;;;;;;;iiiii111iiiiiii;:::::::::::::::::::
+:,,,,,,,,,,,,,,,,:::::::::::::::::::::::,CCffftttt1iii;;i;;;;;;;;iiiiii111iiiiii;:::::::::::::::::::
+:,,,,,,,,,,,,,,,,:::::::::::::::::::::::.fCffftttt11iiiiii;;;;;;;iiiiiiii11;;iii;,,,:::::::::,,,,,::
+:::,,,,,,,,,,,,,,,,:::::::::::::::::::,,,LCffffttt111iiiiiiiii;;;;;iiiiiii1:.:i1i,,,,,,,,::::,,,,,,:
+;;;;;;:::::::::,,,,,,,,,,,,,,,,,,,,,,,,.18CLfttttt1111iiiiiiiii;;;iiiiiii1;..,,:1:,,,,,,,,,:::,,,,,:
+;;;;;::;;;:::::::,,,,,,,,,,,,,,,,,...,,,CGCCftttttt111iiiiiiiii;;;iiiiiii;....,.i1;:,,,,,,:::,,,,,,:
+;;::::::::::::::::,:::::::::::,,.,:if;,,LGCCfttttttt111iii111ii;;;iiiiii: .....,1111i;:,,,,,,,,,,,,,
+;:::::::::::::;;;;::;:;:::::::;1fCCGf,,,tGCLLt11tttt1111i1111iii;;ii;;;, .... .i1ii1111ii:,,,,,,,,,:
+::::::::::::::;;;;;;:::::i1fCG000GCC;,,.iGCCLt111ttt1111111ttii;;;;ii;.  .   ,iiiiiiii11111ii;:::::;
+;;;;:::::::::;;:::::i1fCG888800GCCLC;,,.:LCCLt111tttt11111tt1ii;;;i;:. .    ,iiiiiiiiii111111111ii;;
+;::;:::::;;;:::;itC0888880000GGGCLLCi.,,.iLCLt1111ttt111tttt1ii;i;:.      .:1iiiii11iiii111111111t1t
+:::::;;;;;::;tC0888800000000GGGCCLLCt.,,.,tLLf1111111111ttft1ii;;:.   .  ,;iiii1i11111111111111ii11t
+;;::::::,:1C088800000000000000GCCLLLf,,...;fLf1iii1t11tttfft1ii:.      .:iii111111111111111111111111
+;;;:::,,1G88000000000000000000GCLLLLL;.,..:fLt1iii11ttttfLft1i,. ..  .:i1111111111111111111111111111
+i;;::,;C8800G00GGGGGGGG00000GGCCCLLLLf,....tLt11ii11ttttLLLfi, ..  .:itttt11tt11t111111111ttttt1tt1t
+i;;:,t8800GGGGGGGGGCGGGGGGGGGGGCCLLLLCt....,ff11111tttffLCt:...  ,itttttttttttttttttt1t1t11tttt1tt1t
+;;;,L@0000GGGGGGGGGGCGGGGGGGGGGCCLLLLLCt....,tt111ttffLCLi... .:1tftttttttttttt1111111111111tt111ttt
+;;:L@000GGGGGGGGGGGGGGGGG0GGGGCCLLLLLLfLt:....1tttttfLL1,  ..;1ffttttttttt111111111111111111tttttttt
+i:f80000GGGCCCCCCCCCGG000000GGCCLLLLLLffff1:. .;ttfLf1, .,:1tfttttttttttttt11111111111111ttttttttttt
+;1800GGGGCCCCCCCCCCGGG000000GGCCLLLfLLfffffft;, ,11;,..:itfffttttttttttttttt11111111111ttttttttttttt
+;iL00GGGCCCCCCCCCCCGGG00000GGGCCLLLLLffffffffft1:i;,;1tfffffffffttttttttttttttttt1111ttttttttttttt1t
+;;;1LGGCCCLLLLLLCCGGG0000000GGGCLLLLLfffffftfffttiii;1tffffffffftfttttttttttttttt1111tttttttttttt11t
+i;;::itCCCLLLLLLCCGG00000000GGCCLLLLffffffffft1t1i;;;ii1fffffffffttttttttttttttt1tt111tttttttt1tt11t
+;;;::::;1fLLLLLLLCG0000000GGGGCCCLLLLffffftftt11i::;:;;ifffffffffffftttttttttttt1t11111t11ttt11t1111
+;:::::::::;i1ttLCGGGCCCCGGGGGGCCCLLLfffffftft1i;::::::;1ffffffffffffftttttttttttt1111111111111111111
+;:::::,,,,,,,::;tf1ii;iiii1tfLCCCLLLffffffftti;::::::;1tfffffffffftffffffttttttttt11111111111111111t
+;::::::,,,,,,,,:;iii;;;;;;;;:;i1tLLLffffffffttiiiiii1tffffffffffffffttt111iiiiii1i1111111i1111111111
+:,,,,,,,,,,,,,:;;iiii;;;;;;;:::::;i1ffftffftttttffffffffffffffft11i;::::::,::::::,,::;ii11ii;;;;;;::
+;:::::,,,,,,,.:;iii;;;ii;;;;::::;;:::itfttttttfffffffffffLft1;;:::::::::::::::,,,,,,,,,,::,,.,..,..,
+::,,,,,,,,,,..,;:,,,,,,,,::;;;;;:::::::;1tttttfffffffLLfti;:,::::::::::::::::,,,,,,,,,,,,,,,...,.,.,
+:,,,,,,.,,,....,,,,,,,,.....,::;::::::,,:;1tttfffffLLti;:::::::::::::::::::,,::,::::,,,,,,,,,....,,,
+:,,,,,,::::::::,,:,,.,..........:;;::::::,:i1tttfff1;:::::::::;::,,,...............,,,,,,,,,,,,..,,,
+;:::;;i11iii;;;:,,,,,,...........,:;::::::,:;1ttft;::::::::::,..                       ...,,,,,....,
+;;ii111t11iii;:,,,.,...............,::::::,:::i1i;::::::;:,. . .  .                        ........,
+i1111ttftt11ii;,,,,,.,,..............::::::,:::;::;::;;:,.... ....   .  .....  ...  ....     ....,,,
+fffffLftftt11i;:;,:,:,,,,,..,. ., .:,.,;i:;ttffft1ii;;..,,,.,.:,.:.  ,.  .,.,..,,,.,.,,.,,. .:.,,,::
+LfLLLfffftt11i;ii;;,:,,,,,..,...,..,,..,:fCLLffLfftti:.,....,....,. .,   .. .,........,.,....,.,.,,:
+''')
+print("Welcome to the game \"Save the princess\".\n")
+print("Your mission is to save the princess.\n") 
+print('''You're in front of a mansion.The only way to enter the mansion is by typing yes or no on your phone.What will you type?\n''')
+entry = input('Type "yes" or "no"\n').lower()
+if entry == 'yes':
+  print('''You have entered the mansion.There is a map and a key in front of you.What will you choose?\n''')
+  choose = input("Type 'map' to get the map.Type 'key' to get the key.\n").lower()
+  if choose == 'map':
+    print('''Solve this puzzle correctly to save your princess in time.\n''')
+    print('''Puzzle: What is (2*2)-2/2+2 = ? Type your answer\n''')
+    num = int(input("Type your answer\n"))
+    if num == 1:
+      print("You have found your princess.Its time to celebrate.YAYYYY...YOU WIN\n")
+    else:
+      print("You are teleported to a room full of lions.Game Over.\n")
+  else:
+    print('''You have been teleported to another dimension. To save your princess in time,solve this puzzle correctly.\n''')
+    print('''Puzzle: What can be standing and going, standing and hanging on, going and lying down at the same time? What is your answer. A)clock B)Life C)train\n''')
+    answer = input("Type 'A' or 'B' or 'C'\n").lower()
+    if answer == 'A'.lower():
+      print("You have found your princess.Its time to celebrate.YAYYYY...YOU WIN\n")
+    else:
+      print("You are teleported to a room full of lions.Game Over.\n")
+      
+else:
+  print("You are eaten by zombies.Game over.\n")
