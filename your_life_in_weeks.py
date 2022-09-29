c8feea0808b6425ae94ae41fc5087d4d6149fddd
Anusha <divakardivi1963@gmail.com>
2022-09-29 14:14:56 +0530

    Revert "your_life_in_weeks.py file removed"
    This reverts commit f68f27b434f8dbf3cd232a6fd60cc4c35652c721.

---
+++ your_life_in_weeks.py
@@ -0,0 +1,8 @@
+# YOUR LIFE IN WEEKS
+age = input("What is your current age?")
+n = 90 - int(age)
+days = n*365
+weeks = n*52
+months = n*12
+# print(type(n))
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
